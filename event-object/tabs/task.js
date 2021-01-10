92b66ecfa7ec5c4e918ef48489c312a73f281713
unknown <evgn09@yandex.ru>
2021-01-10 21:59:00 +0300

    исправлено tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,23 +1,15 @@
-let counter = 0;
-let tabActive = 0;
-let tabContentActive = 0;
-const menu = document.body.querySelectorAll(".tab");
-for (let i = 0; i < menu.length; i++) {
-    menu[i].addEventListener("click", function () {
-        this.classList.add("tab_active");
-        const content = document.body.querySelectorAll('.tab__content');
-        if (counter >= 1) {
-            tabActive.remove("tab_active");
-            tabContentActive.remove("tab__content_active");
-            if (counter > 3) {
-                counter = 0;
-                tabContentActive = 0;
-            }
+let nav = Array.from(document.getElementsByClassName("tab"));
+let content = Array.from(document.getElementsByClassName("tab__content"));
 
-        }
-        content[i].classList.add('tab__content_active');
-        tabContentActive = content[i].classList;
-        tabActive = this.classList;
-        counter += 1;
-    })
-}
+nav.forEach(element => {
+  element.addEventListener('click', function () {
+      for (let el of nav) {
+          el.className = "tab";
+      }
+      for (let el of content) {
+          el.className = "tab__content";
+      }
+      element.classList.add("tab_active");
+      content[nav.indexOf(element)].classList.add("tab__content_active");
+  })
+})
